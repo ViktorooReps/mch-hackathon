7e29c3de89a585acd415fd42abf384d3e36ab8aa
viktoroo <viktoroo.sch@gmail.com>
2022-06-11 13:39:37 +0300

    html with selenium

--- crawlers/mos.py
+++ crawlers/mos.py
@@ -1,8 +1,8 @@
 from time import sleep
 from typing import Iterable, Dict, Any
 
-from newspaper import Article
-from requests import get
+from requests import get, post
+from selenium import webdriver
 
 
 class RequestBuilder:
@@ -27,18 +27,19 @@ def get_urls(*, timeout: float = 1.0) -> Iterable[str]:
     request_builder = RequestBuilder('https://www.mos.ru/search?')
     args = {
         'category': 'newsfeed',
-        'q': '',
         'skip_stat': 2,
         'spheres': 14299,
         'types': 'news'
     }
-    for page in range(1, 44):
+    driver = webdriver.Chrome()
+    for page in range(41, 47):
         print(f'Crawling page {page}...')
         args['page'] = page
 
         url = request_builder.build(args)
-        response = get(url)
-        yield from _get_urls_from_html(response.text)
+        driver.get(url)
+        text = driver.page_source
+        yield from _get_urls_from_html(text)
 
         sleep(timeout)
 
