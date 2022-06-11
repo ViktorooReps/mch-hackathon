8f7694e411933e738a6d7a4f0ef92651925af8a2
viktoroo <viktoroo.sch@gmail.com>
2022-06-11 11:46:11 +0300

    bump python version

--- init.sh
+++ init.sh
@@ -5,6 +5,6 @@ sudo apt-get install libjpeg-dev zlib1g-dev libpng12-dev
 curl https://raw.githubusercontent.com/codelucas/newspaper/master/download_corpora.py | python3
 
 pip install virtualenv
-virtualenv -p /usr/bin/python2.7 venv
+virtualenv -p /usr/bin/python3.10 venv
 source venv/bin/activate
 pip install -r requirements.txt
