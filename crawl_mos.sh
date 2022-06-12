604bbf255c8411f136f2fe53322b061db1937186
viktoroo <viktoroo.sch@gmail.com>
2022-06-13 01:06:08 +0300

    small crawler refactoring

--- crawl_mos.sh
+++ crawl_mos.sh
@@ -1,1 +1,2 @@
-python3 crawl_mos.py --timeout=0.0 --patience=30 --from_date=1999-1-1 --to_date=2025-1-1 --out_path=data/mos/data.jsonl
+export PYTHONPATH=.
+python3 crawlers/mos/crawl_mos.py --timeout=0.0 --patience=30 --ignore_date --out_path=data/mos/2020all.jsonl
