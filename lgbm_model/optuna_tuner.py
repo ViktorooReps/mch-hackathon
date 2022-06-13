54daee4858e54d31877fe5c657fe608d9ef232d4
viktoroo <viktoroo.sch@gmail.com>
2022-06-13 05:13:23 +0300

    finished dataset loading

--- lgbm_model/optuna_tuner.py
+++ lgbm_model/optuna_tuner.py
@@ -1,3 +1,7 @@
+import pickle
+
+import numpy as np
+
 from abs_optuna_tuner import AbstractOptunaTuner
 from sklearn.datasets import load_breast_cancer
 
@@ -12,8 +16,10 @@ class OptunaTuner(AbstractOptunaTuner):
             Для тренировки нужно заимплементить этот метод, - 
             он должен возвращать X_train, y_Train, X_valid, y_valid
         """
-        X, y = load_breast_cancer(return_X_y=True)
-        border = 300
+        with open('cached.pkl', 'rb') as f:
+            X, y = pickle.load(f)
+
+        border = int(0.2 * len(y))
 
         X_train, X_valid = X[:border], X[border:]
         y_train, y_valid = y[:border], y[border:]
