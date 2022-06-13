cc8210e68669813521eb9ddd8c1e138a7b3c3d91
Eduard Martynov <57013219+dazzle-me@users.noreply.github.com>
2022-06-13 13:17:56 +0300

    make embedder CPU only

--- bank_embeddings/feature_extraction/bert.py
+++ bank_embeddings/feature_extraction/bert.py
@@ -1,51 +1,29 @@
-from torch import Tensor
 from transformers import AutoTokenizer, AutoModel, pipeline
 import torch
+import numpy as np
 
+## INFERENCE CPU ONLY!
 class FeatureExtractor:
-    """
-    Класс для извлечения BERT embeddings из текста
-    Args:
-        model_name: Название модели трансформера для извлечения эмбеддингов
-        token_size: Кол-во токенов, которое будет передаваться модели для дальнейшего 
-        извлечения эмбеддингов
-        device: Процессор, на котром будет работать BERT-модель
-    """
-    def __init__(self, model_name="DeepPavlov/rubert-base-cased", token_size=512, device='cpu'):
+    def __init__(self, model_name="DeepPavlov/rubert-base-cased", token_size=512):
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.model = AutoModel.from_pretrained(model_name)
         self.token_size = token_size
-        self.device = device
         self.pipeline = self.make_pipeline()
 
     def make_pipeline(self):
-        """
-        Метод класса, собирающий токенайзер и модель в общий пайплайн
-        """
         def cut_token(token):
-            """
-            Функция, обрезающая токены до размера token_size
-            """
             for key in token:
                 token[key] = token[key][:, :self.token_size]
             return token
 
         def tokenizer_with_cut(text, return_tensors=None):
-            """
-            Функция, возвращающая токенайзер для пайплайна
-            """
             return cut_token(self.tokenizer(text, return_tensors=return_tensors))
 
-        return pipeline('feature-extraction', tokenizer=tokenizer_with_cut, model=self.model, device=0)
+        return pipeline('feature-extraction', tokenizer=tokenizer_with_cut, model=self.model)
 
     def extract_features(self, text):
-        """
-        Метод класса, извлекающий BERT признаки из текста
-        Args:
-            text: Текст для извлечения признаков
-        """
-        result = torch.FloatTensor(self.pipeline(text))[0, 0, :]
-        # print(result.shape)
+        embedding = self.pipeline(text)
+        result = torch.FloatTensor(embedding)[0, 0, :]
         return result
 
 
