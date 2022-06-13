6deb9c288250b829c94922ecfd346724f05a378b
Eduard Martynov <57013219+dazzle-me@users.noreply.github.com>
2022-06-13 13:22:14 +0300

    add prepare_embeddings method inside Comparator class

--- bank_embeddings/comparator.py
+++ bank_embeddings/comparator.py
@@ -8,13 +8,33 @@ warnings.filterwarnings("ignore")
 from feature_extraction.bert import FeatureExtractor
 
 class Comparator():
-    def __init__(self, dataframe: pd.DataFrame, title_embeddings: np.array, text_embeddings: np.array):
+    def __init__(self, data_dir):
+        dataframe, title_embeddings, text_embeddings = self.prepare_embeddings(data_dir)
+        
         self.source_meta = dataframe
         
         self.title_source_embeddings = title_embeddings
         self.text_source_embeddings = text_embeddings
 
         self.embedder = FeatureExtractor()
+
+    def prepare_embeddings(self, data_dir):
+        train_meta = pd.read_csv(join(data_dir, f'train.csv'))
+        train_text_embeddings = np.load(join(data_dir, f'train_text_bank_embeddings.npy'))
+        train_title_embeddings = np.load(join(data_dir, f'train_title_bank_embeddings.npy'))
+
+        test_meta = pd.read_csv(join(data_dir, f'test.csv'))
+        test_text_embeddings = np.load(join(data_dir, f'test_text_bank_embeddings.npy'))
+        test_title_embeddings = np.load(join(data_dir, f'test_title_bank_embeddings.npy'))
+
+        meta = pd.concat([train_meta, test_meta]).reset_index(drop=True)
+        text_embeddings = np.concatenate([train_text_embeddings, test_text_embeddings], axis=0)
+        title_embeddings = np.concatenate([train_title_embeddings, test_title_embeddings], axis=0)
+        print(f"Meta-information length : {len(meta)}")
+        print(f"Text embeddings shape : {text_embeddings.shape}")
+        print(f"Title embeddings shape : {title_embeddings.shape}")
+        
+        return meta, title_embeddings, text_embeddings
 
     def get_source(self, text: str, *, top_k: int = 1, use_title: bool = False):
         embedding = np.array(self.embedder.extract_features(text))
@@ -30,35 +50,12 @@ class Comparator():
         indices = np.argsort(similarity)
         indices = indices[-top_k:]
         return indices, similarity
-
+    
 if __name__ == "__main__":
-    dataset = 'train+test'
     use_title = False
     top_k = 50
     data_dir = 'input'
-
-    if dataset == 'test' or dataset == 'train':
-        meta = pd.read_csv(join(data_dir, f'{dataset}.csv'))
-        text_embeddings = np.load(join(data_dir, f'{dataset}_text_bank_embeddings.npy'))
-        title_embeddings = np.load(join(data_dir, f'{dataset}_title_bank_embeddings.npy'))
-    else:
-        train_meta = pd.read_csv(join(data_dir, f'train.csv'))
-        train_text_embeddings = np.load(join(data_dir, f'train_text_bank_embeddings.npy'))
-        train_title_embeddings = np.load(join(data_dir, f'train_title_bank_embeddings.npy'))
-
-        test_meta = pd.read_csv(join(data_dir, f'test.csv'))
-        test_text_embeddings = np.load(join(data_dir, f'test_text_bank_embeddings.npy'))
-        test_title_embeddings = np.load(join(data_dir, f'test_title_bank_embeddings.npy'))
-
-        meta = pd.concat([train_meta, test_meta]).reset_index(drop=True)
-        text_embeddings = np.concatenate([train_text_embeddings, test_text_embeddings], axis=0)
-        title_embeddings = np.concatenate([train_title_embeddings, test_title_embeddings], axis=0)
-
-    print(f"Meta-information length : {len(meta)}")
-    print(f"Text embeddings shape : {text_embeddings.shape}")
-    print(f"Title embeddings shape : {title_embeddings.shape}")
-    
-    comparator = Comparator(meta,  title_embeddings, text_embeddings)
+    comparator = Comparator(data_dir)
     
     fakes = pd.read_csv(join(data_dir, 'provided_fakes.csv'))
     # for test_index in range(len(fakes)):
